ce401d42c8bd69332ff278e58e9284eb817c106e
Ilinca Hogiu <ilincahogiu@gmail.com>
2024-06-02 18:17:59 +0300

    finished login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import './Login.css';
 import { FaLock, FaEnvelope } from "react-icons/fa";
+import axios from 'axios';
 
 export default function Login() {
 
@@ -10,20 +11,90 @@ export default function Login() {
         setAction('active');
     };
 
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const [errors, setErrors] = useState ({})
+    const [valid, setValid] = useState(true)
+
+    const handleSubmit= (e) => {
+        e.preventDefault();
+        let isValid = true;
+        let validationErrors = {}
+
+        if(formData.email === "" || formData.email === null) {
+            isValid = false;
+            validationErrors.email = "E-mail is required"
+        } else if(!/\S+@\S+\.\S+/.test(formData.email))  {
+            isValid = false;
+            validationErrors.email = "E-mail format is not valid"
+        }
+
+        if(formData.password === "" || formData.password === null) {
+            isValid = false;
+            validationErrors.password = "A password is required"
+        } else if(formData.password.length < 6)  {
+            isValid = false;
+            validationErrors.password = "Password length: at least 6 characters"
+        }
+
+        setErrors(validationErrors)
+        setValid(isValid)
+
+        axios.get('http://localhost:8000/users')
+        .then(result => {
+            const user = result.data.find(user => user.email === formData.email);
+    
+            if (user) {
+                if (user.password === formData.password) {
+                    alert("Successfully logged in");
+                } else {
+                    isValid = false;
+                    validationErrors.password = "Wrong password";
+                }
+            } else {
+                isValid = false;
+                validationErrors.email = "This e-mail is not registered";
+            }
+        })
+        .catch(err => console.log(err));
+    
+    }
+
     return (
         <>
             <div className={`wrapper ${action}`}>
                 <div className='form-box login'>
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit}>
                         <h1>Autentificare</h1>
+                        {
+                            valid ? <></> :
+                            <span className="text-danger">
+                                {errors.email}; {errors.password};
+                            </span>
+                        }
                         <div className='input-box'>
-                            <input type="email" placeholder='E-mail' required />
+                            <input 
+                                type="email" 
+                                name="email"
+                                placeholder='E-mail' 
+                                onChange={(event) => setFormData({...formData, email: event.target.value})}
+                                 />
                             <FaEnvelope className='icon'/>
                         </div>
+
                         <div className='input-box'>
-                            <input type="password" placeholder='Password' required />
+                            <input 
+                                type="password" 
+                                name="password"
+                                placeholder='Password' 
+                                onChange={(event) => setFormData({...formData, password: event.target.value})}
+                                 />
                             <FaLock className='icon'/>
                         </div>
+                        
                         <div className="remember-forgot">
                             <label><input type="checkbox"/>Ține-mă minte </label>
                             <a href="#">Ai uitat parola?</a>
